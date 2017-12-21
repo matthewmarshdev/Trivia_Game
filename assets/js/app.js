0cbcbba48952916ed5f552842e4437e94d2bfda9
Matthew <matthewmarshdev@gmail.com^C>
2017-12-20 17:11:54 -0700

    commits from stellas at 5:11

--- assets/js/app.js
+++ assets/js/app.js
@@ -26,61 +26,71 @@ or negative message **/
 
 var questionsAndAnswers = [
 	{
-	question1: 'Which final fantasy debuted in 1997?', 
-	answer1X: 'Final Fantsasy VII',
-	answer2: 'Final Fantasy VIII',
-	answer3: 'Final Fantasy Tactics',
-	answer4: 'Final Fantasy V'
+	'question1': 'Which final fantasy debuted in 1997?', 
+	'answer1X': 'Final Fantsasy VII',
+	'answer2': 'Final Fantasy VIII',
+	'answer3': 'Final Fantasy Tactics',
+	'answer4': 'Final Fantasy V'
 	},
 	{
-	question1: 'What SNES game was ported from japan in 1994', 
-	answer1X: 'Mother II',
-	answer2: 'Legend of Mana',
-	answer3: 'Secret of Gaia',
-	Answer4: 'Harvest Moon'
+	'question1': 'What SNES game was ported from japan in 1994', 
+	'answer1X': 'Mother II',
+	'answer2': 'Legend of Mana',
+	'answer3': 'Secret of Gaia',
+	'answer4': 'Harvest Moon'
 	},
 	{
-	question1: 'Which of the following was an Easter Egg from "Halo: Combat Evolved Anniversary Edition"?', 
-	answer1X: 'Troll Meme In Pillar of Autumn Message board',
-	answer2: 'Grunt headshots make screaming noise',
-	answer3: 'Elites hugged each other at end',
-	Answer4: 'Chief curses if his gun is jammed'
+	'question1': 'Which of the following was an Easter Egg from "Halo: Combat Evolved Anniversary Edition"?', 
+	'answer1X': 'Troll Meme In Pillar of Autumn Message board',
+	'answer2': 'Grunt headshots make screaming noise',
+	'answer3': 'Elites hugged each other at end',
+	'Answer4': 'Chief curses if his gun is jammed'
 	},
 	{
-	question1: 'What do you get from the secret gnome house in year 1 of Harvest Moon?', 
-	answer1: 'Golden Hoe',
-	answer2X: 'Golden Sickle',
-	answer3: 'Golden Axe',
-	Answer4: 'Golden Sprinkler'
+	'question: 'What do you get from the secret gnome house in year 1 of Harvest Moon?', 
+	'answer1': 'Golden Hoe',
+	'answer2X': 'Golden Sickle',
+	'answer3': 'Golden Axe',
+	'Answer4': 'Golden Sprinkler'
 	},
 	{
-	question1: 'How many flutes do you need to skip to work 8 from world 1 in Super Mario Brothers 3?', 
-	answer1: '1',
-	answer2X: '2',
-	answer3: '3',
-	Answer4: '4'
+	'question': 'How many flutes do you need to skip to work 8 from world 1 in Super Mario Brothers 3?', 
+	'answer1': '1',
+	'answer2X': '2',
+	'answer3': '3',
+	'Answer4': '4'
 	},
 	{
-	question1: 'Who is Links Nemesis?', 
-	answer1: 'Ganon',
-	answer2: 'Bowser',
-	answer3: 'Final Fantasy Tactics',
-	Answer4: 'Final Fantasy V'
+	'question': 'Who is Links Nemesis?', 
+	'answer1': 'Ganon',
+	'answer2': 'Bowser',
+	'answer3': 'Final Fantasy Tactics',
+	'Answer4': 'Final Fantasy V'
 	}
 ];
+
+$question = $('.question');
+$answer1 = $('answer1');
+$answer2 = $('answer2');
+$answer3 = $('answer3');
+$answer4 = $('answer4');
+$score = $('.score')
+
+counter = 0;
+score = 0;
 
 
 
 function displayQusetion(){
-	$('#question').html(questionsAndAnswers[0].question1)
-	$('#Answer1').html(questionsAndAnswers[0].answer1X);
-	$('#Answer2').show(questionsAndAnswers[0].answer2);
-	$('#Answer3').add(questionsAndAnswers[0].answer3);
-	$('#Answer4').append(questionsAndAnswers[0].Answer4);
+	$('.question').html(questionsAndAnswers[0].question1)
+	$('.answer1').html(questionsAndAnswers[0].answer1X);
+	$('.answer2').show(questionsAndAnswers[0].answer2);
+	$('.answer3').add(questionsAndAnswers[0].answer3);
+	$('.answer4').append(questionsAndAnswers[0].Answer4);
 
 };
 
-function
+
 
 function displayScore(){
 
